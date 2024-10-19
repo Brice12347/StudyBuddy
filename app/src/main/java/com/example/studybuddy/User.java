a0d57768b77e097e6d5b205f174a21328c3ac556
Brice Patchou <108960259+Brice12347@users.noreply.github.com>
2024-10-18 18:47:43 -0700

    created pages and User Class

---
+++ app/src/main/java/com/example/studybuddy/User.java
@@ -0,0 +1,21 @@
+package com.example.studybuddy;
+
+import java.util.ArrayList;
+
+public class User {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    private String password;
+
+    ArrayList<String> profileInfo;
+
+
+}
