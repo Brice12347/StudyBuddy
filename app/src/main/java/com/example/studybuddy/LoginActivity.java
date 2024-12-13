45f9a1d6ae0aebfe0067fcd022a525a79f317dda
Jonathan-Aydin <gaboroaydin@gmail.com>
2024-12-13 15:13:52 -0800

    fix replacement of LoginActivity

---
+++ app/src/main/java/com/example/studybuddy/LoginActivity.java
@@ -0,0 +1,125 @@
+package com.example.studybuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout loginUsername, loginPassword;
+    Button loginButton;
+    Button backButton;
+    TextView signupRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.login_page);
+        loginUsername = findViewById(R.id.loginUsernameInput);
+        loginPassword = findViewById(R.id.loginPassInput);
+        loginButton = findViewById(R.id.loginPageLoginButton);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!validateUsername() | !validatePassword()) {
+                } else {
+                    checkUser();
+                }
+            }
+        });
+
+        // Initialize Back Button
+        backButton = findViewById(R.id.backButton);
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("username", getIntent().getStringExtra("username")); // Pass username
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+
+
+
+
+    }
+
+    public Boolean validateUsername() {
+        String val = loginUsername.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            loginUsername.setError("Username cannot be empty");
+            return false;
+        } else {
+            loginUsername.setError(null);
+            return true;
+        }
+    }
+    public Boolean validatePassword(){
+        String val = loginPassword.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            loginPassword.setError("Password cannot be empty");
+            return false;
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String userUsername = loginUsername.getEditText().getText().toString().trim();
+        String userPassword = loginPassword.getEditText().getText().toString().trim();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    loginUsername.setError(null);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+                    if (passwordFromDB.equals(userPassword)) {
+                        loginUsername.setError(null);
+                        String usernameFromDB = snapshot.child(userUsername).child("username").getValue(String.class);
+                        Intent intent = new Intent(LoginActivity.this, LandingPageActivity.class);
+                        //intent = new Intent(LoginActivity.this, StudyGroupActivity.class);
+                        intent.putExtra("username", usernameFromDB);
+                        intent.putExtra("password", passwordFromDB);
+                        startActivity(intent);
+                    } else {
+                        loginPassword.setError("Invalid Credentials");
+                        loginPassword.requestFocus();
+                    }
+                } else {
+                    loginUsername.setError("User does not exist");
+                    loginUsername.requestFocus();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+
+}
